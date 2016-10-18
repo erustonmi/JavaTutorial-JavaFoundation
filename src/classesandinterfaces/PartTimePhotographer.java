80ad2ed5f64782ae8ee2bf5bebcec70ecae8d2a6
SimonHGR <simon@dancingcloudservices.com>
2016-10-18 10:42:31 -0400

    instance initialization block added

--- src/classesandinterfaces/PartTimePhotographer.java
+++ src/classesandinterfaces/PartTimePhotographer.java
@@ -2,6 +2,11 @@ package classesandinterfaces;
 
 public class PartTimePhotographer extends ProfessionalPhotographer {
 	private int hoursPerWeek;
+	{
+		for (int i = 0; i < 10; i++) {
+			hoursPerWeek += (int)(Math.random() * 2);
+		}
+	}
 	
 	public PartTimePhotographer(String name) {
 		super(name);
